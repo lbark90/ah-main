66e70274bc251acb2939fdb7053ac3838ca0dda5
Larry Bark <larry@woofconsultingmd.com>
2025-05-01 14:52:18 -0400

    fixed for VM

--- app/api/photos/[...params]/route.ts
+++ app/api/photos/[...params]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { Storage } from '@google-cloud/storage';
 import path from 'path';
 import fs from 'fs';
